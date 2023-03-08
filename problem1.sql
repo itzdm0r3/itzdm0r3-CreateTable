c79cc0e3cb117f37ee92a6ea65dcf22280fe1fbe
Dwayne Moreland <dmoreland84@gmail.com>
2023-03-08 02:58:02 +0000

    Completed Lab CreateTable

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,9 @@
+/* 1. create table called SONG.
+   2. create two columns for "title" and "artist".
+   3. give the columns the datatype of varchar(100), meaning a string of 100 characters.
+*/
 
+CREATE TABLE song (
+    title varchar(100) not null,
+    artist varchar(100) not null
+);  
